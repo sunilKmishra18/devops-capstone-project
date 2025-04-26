8844189e0861f9262768361a45ecc4950b432780
Sunil Mishra <sunilmishra18@gmail.com>
2025-04-26 20:04:18 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -71,7 +71,7 @@ def list_accounts():
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
-
+    
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
