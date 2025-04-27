57e05ce8adb05e193ee12f51239bbbd04a9df7f3
Sunil Mishra <sunilmishra18.creativity@gmail.com>
2025-04-27 07:02:55 -0400

    Added security headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,12 +12,17 @@ from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
+from service import talisman
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
 
 BASE_URL = "/accounts"
+
+# To get the Flask test client to use https with the environ_overrides attribute.
+# When making an URL call, pass environ_overrides=HTTPS_ENVIRON
+HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
 
 ######################################################################
@@ -34,6 +39,9 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
+        # Talisman will force all requests to your REST API to use the https:// protocol.
+        # This is a good thing, except perhaps when testing (-> all tests will fail).
+        talisman.force_https = False
 
     @classmethod
     def tearDownClass(cls):
@@ -85,6 +93,19 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, 200)
         data = resp.get_json()
         self.assertEqual(data["status"], "OK")
+
+    def test_security_headers(self):
+        """It should return security headers"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        headers = {
+            'X-Frame-Options': 'SAMEORIGIN',
+            'X-Content-Type-Options': 'nosniff',
+            'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+            'Referrer-Policy': 'strict-origin-when-cross-origin'
+        }
+        for key, value in headers.items():
+            self.assertEqual(response.headers.get(key), value)
 
     def test_create_account(self):
         """It should Create a new Account"""
